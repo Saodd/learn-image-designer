3bcf93c2c51df22ce71eee494b8a8ff158cd5d08
Lin Lan <lanlin@meideng.net>
2022-04-25 14:01:04 +0800

    init from template

---
+++ webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config.js');
+const config = require('./package.json');
+const webpack = require('webpack');
+
+const now = new Date();
+config.version = `0.${(now.getMonth() + 1) * 100 + now.getDate()}.${now.getHours() * 100 + now.getMinutes()}`;
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      __NPM_VERSION__: JSON.stringify(require('./package.json').version),
+    }),
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    client: {
+      webSocketURL: 'ws://localhost:3000/ws',
+    },
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+    allowedHosts: 'all',
+  },
+});
